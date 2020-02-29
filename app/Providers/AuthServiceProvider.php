3a81fcc78199dd51b15e89afed17e76df46c23a1
Happeace304 <happeace304@gmail.com>
2020-02-29 15:28:43 +0700

    02292020 config heroku db

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
-        //
+        Passport::loadKeysFrom(app_path().'\secret-keys');
     }
 }
