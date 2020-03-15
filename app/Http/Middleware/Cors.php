0cb2b9ac1390a55c8ff6fea3e413b8b0983cab19
Happeace304 <happeace304@gmail.com>
2020-03-15 18:45:01 +0700

    0315 cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,18 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header("Access-Control-Allow-Origin", "*")
-            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-            ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+        header("Access-Control-Allow-Origin: *");
+        $headers = [
+            "Access-Control-Allow-Methods" => "POST, GET, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers" => "Content-Type, X-Auth-Token, Origin, Authorization"
+        ];
+        if($request->getMethod() == 'OPTIONS') {
+            return response()->json('OK', 200, $headers);
+        }
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header ($key, $value);
+        }
+        return $response;
     }
 }
