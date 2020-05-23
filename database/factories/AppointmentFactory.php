40275131455f352ac9be535ab1a2d93534a6d34a
Happeace304 <happeace304@gmail.com>
2020-05-23 16:32:09 +0700

    0523 update appointment factory

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -5,7 +5,7 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Appointment::class, function (Faker $faker) {
-    $date = \Carbon\Carbon::create(2020, 5, 19+rand(0,10), 8, 0, 0);
+    $date = \Carbon\Carbon::create(2020, 5, 19+rand(0,10), 8+rand(0,8), 5*rand(0,12), 0);
     return [
         'remark'=> $faker->realText(50),
         'start_time'=> $date->format('Y-m-d H:i:s'),
